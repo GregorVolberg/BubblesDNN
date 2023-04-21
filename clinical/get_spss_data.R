5b7b96c5c16ddb76cb1ee3c9683a83cd3165a40d
Gregor Volberg <vog20246@pc1032250291.ads.uni-regensburg.de>
2023-04-21 13:46:04 +0200

    Clinical

---
+++ clinical/get_spss_data.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(haven)
+
+## run from directory [/loctmp2/data/vog20246/BubblesDNN/clinical] 
+## or X:/BubblesDNN/clinical'
+protocolFile  <- '../img/dsetComposite/BubblesProtocolComposite2.txt'
+spssFile      <- './Bubbles_SPSSDatei_30.03.2023.sav'
+
+vplist <- read.csv(protocolFile, sep = '\t', header = F) %>%
+  as_tibble() %>%
+  select(vp = V2, group = V5) %>%
+  mutate(vp = str_replace(vp, 'S', '0')) %>% # convert to three digit code
+  unique() %>%
+  arrange(vp) 
+
+spss  <- read_sav(spssFile, encoding = 'latin1') %>%
+  select(vp=VPNummer, age=Alter, groupSPSS=Kontrollgruppe_vs_NSSI, iq=IQPunkte, hand=`HÃ¤ndigkeit`)
+
+vplist$vp
